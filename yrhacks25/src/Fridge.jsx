50d5411a341be9eeae2ce4b45e964af6067a33f2
ChristianCav <christiancav55@gmail.com>
2025-04-08 09:47:36 -0400

    fridge components

---
+++ yrhacks25/src/Fridge.jsx
@@ -0,0 +1,32 @@
+import Food from './Food';
+import Category from './Category';
+
+function Fridge({ foodItems }) {
+  return (
+    <div className="fridgeGridContainer">
+      <div className="categorySection">
+        <Category name="Fruit" />
+        <Category name="Vegetables"/>
+        <Category name="Meat"/>
+        <Category name="Dairy"/>
+        <Category name="Seafood"/>
+        <Category name="Grains"/>
+        <Category name="Legumes"/>
+        <Category name="Beverages"/>
+        <Category name="Baking"/>
+        <Category name="Condiments"/>
+        <Category name="Herbs & Spices"/>
+        <Category name="Oils & Fats"/>
+        <div className="fridgeGrid">
+          {foodItems.map((item, index) => (
+            <div key={index} className="foodCategory">
+              <Food name={item.name} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Fridge;
