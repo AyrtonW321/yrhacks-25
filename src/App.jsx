14077f768c5961966e1992c7f1efda066d671ef4
Ayrton Wong <[ayrtonwongg@gmail.com]>
2025-04-08 01:43:37 -0400

    Merge barcode and mainpage together

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './components/feature/NavBar';
 import MainPage from "./components/pages/MainPage";
 // import Recipe from './components/pages/Recipe';
 // import Fridge from './components/pages/Fridge';
-// import ShoppingList from './Components/pages/ShoppingList';
+import ShoppingList from "./components/pages/ShoppingList";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage/>} />
         {/* <Route path="/recipe" element={<Recipe/>}/>
-        <Route path="/fridge" element={<Fridge/>}/>
-        <Route path="/shoppinglist" element={<ShoppingList/>}/> */}
+        <Route path="/fridge" element={<Fridge/>}/> */}
+        <Route path="/ShoppingList" element={<ShoppingList/>}/>
       </Routes>
     </Router>
   )
